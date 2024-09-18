cdc02355312549409ee7f87d36de75f0dfe82142
Devansh <devansh.arctikcircle@gmail.com>
2024-09-18 06:56:27 +0530

    Langchain Prompts: invoke propmts with chain

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import langchain
 # from langchain_openai import OpenAi
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a world class technical coder."),
+    ("user", "Explain the concepts to me like I am 5 year old"),
+    ("system", "I will explain code like I am explaining to 5 year old"),
+    ("user", "also explain in just 20 words"),
+    ("user", "{input}")
+])
 
 llm = ChatOpenAI(temperature=0.9)
 
-output = llm.invoke("What is google?")
+chain = prompt | llm
+
+output = chain.invoke({"input":"what is internet?"})
 
 print(output)
